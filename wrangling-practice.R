959921f19cb42d43476de2f3fa9f0c93ab05109c
SRKads1998 <kadonoffs@gmail.com>
2025-08-19 11:46:50 -0700

    joins

--- wrangling-practice.R
+++ wrangling-practice.R
@@ -3,6 +3,7 @@ library(palmerpenguins)
 library(lubridate)
 library(ggplot2)
 library(here)
+library(dplyr)
 
 # clear environment rm(list = lS())
 view(penguins)
@@ -27,3 +28,30 @@ penguins %>%
             standard_dev = sd(flipper_length_mm),
             sample_size = n())
             
+animals <- data.frame(
+  stringsAsFactors = FALSE,
+          location = c("lagoon", "bluff", "creek", "oaks", "bluff"),
+           species = c("bobcat", "coyote", "fox", "squirrel", "bobcat"),
+          maturity = c("adult", "juvenile", "adult", "juvenile", "adult")
+)
+sites <- data.frame(
+  stringsAsFactors = FALSE,
+          location = c("beach", "lagoon", "bluff", "oaks"),
+    full_site_name = c("Goleta Beach","UCSB Lagoon",
+                       "Ellwood Mesa","Fremont Campground"),
+      jurisdiction = c("SB City", "UCSB", "SB City", "USFS")
+)
+
+# practice joins
+
+full_join(animals, sites)
+
+left_join(animals, sites)
+
+right_join(animals, sites)
+
+inner_join(animals, sites)
+
+semi_join(animals, sites)
+
+anti_join(animals, sites)
